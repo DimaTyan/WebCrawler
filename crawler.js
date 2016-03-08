f8739e14b0ada203da1a101b0a400a2cc3111dd1
Dimitri Tyan <dimitri.tyan@gmail.com>
2016-03-08 21:19:10 +0100

    added subreddits

--- crawler.js
+++ crawler.js
@@ -11,11 +11,11 @@ request("https://www.reddit.com", function(error, response, body){
 
   $('div#siteTable > div.link').each(function(index){
     var title = $(this).find('p.title > a.title').text().trim();
-    var score = $(this).find('div.score.unvoted').text().trim();
+    var subreddit = $(this).find('a.subreddit').text().trim();
     var user = $(this).find('a.author').text().trim();
 
     console.log("Title: " + title);
-    console.log("Score " + score);
+    console.log("Subreddit " + subreddit);
     console.log("User " + user);
   });
 });
